150b8e3bf8df912eba9795ce46b98e312c13a557
timwis <tim@timwis.com>
2016-10-09 12:42:21 -0400

    use yargs, dotenv, specify percentage

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const prompt = require('prompt')
-const argv = require('minimist')(process.argv.slice(2))
+const yargs = require('yargs')
 const Nightmare = require('nightmare')
 const isSameDay = require('date-fns/is_same_day')
 const format = require('date-fns/format')
 
 const config = require('./config')
 
+const defaultDate = format(Date.now(), 'YYYY-MM-DD')
+
+const argv = yargs
+  .env('CC')
+  .option('username', { alias: 'u', describe: 'Bank account username' })
+  .option('password', { alias: 'p', describe: 'Bank account password (omit for prompt)' })
+  .option('date', { alias: 'd', describe: 'Date of transactions (YYYY-MM-DD)' })
+  .option('from', { alias: 'f', describe: 'Account to transfer from' })
+  .option('to', { alias: 't', describe: 'Account to transfer to' })
+  .option('percentage', { alias: 'percent', describe: 'Percentage to set aside', type: 'number' })
+  .help()
+  .argv
+
 prompt.start()
 
-const promptConfig = [
+const promptSchema = [
   { name: 'username', required: true },
   { name: 'password', required: true, hidden: true },
-  { name: 'date', default: format(Date.now(), 'YYYY-MM-DD') },
-  { name: 'from' },
-  { name: 'to' }
+  { name: 'date', default: defaultDate },
+  { name: 'from', required: true },
+  { name: 'to', required: true },
+  { name: 'percentage', default: 100, type: 'number' }
 ]
 
 prompt.override = argv
 
-prompt.get(promptConfig, (err, input) => {
+prompt.get(promptSchema, (err, input) => {
   if (err) return console.error(err)
 
   const nightmare = Nightmare({ show: true })
@@ -71,21 +85,27 @@ prompt.get(promptConfig, (err, input) => {
       })
       console.log(filteredTransactions)
 
-      const total = filteredTransactions.reduce((accum, transaction) => accum + transaction.amount, 0).toFixed(2)
-      console.log('total', total)
+      const totalSpent = filteredTransactions.reduce((accum, transaction) => accum + transaction.amount, 0).toFixed(2)
+      console.log(`Total spent: ${totalSpent}`)
 
       const formattedDate = format(input.date, 'YYYY-MM-DD')
 
-      if (total < 0) return
+      if (totalSpent < 0) return
+
+      const setAsideAmount = input.percentage !== 100
+        ? ((input.percentage / 100) * totalSpent).toFixed(2)
+        : totalSpent
+      console.log(`Setting aside ${setAsideAmount}`)
 
       return nightmare.goto(config.bank.transfer.url)
-        .type(config.bank.transfer.amount, total)
+        .type(config.bank.transfer.amount, setAsideAmount)
         .type(config.bank.transfer.memo, `CC spending ${formattedDate}`)
         .select(config.bank.transfer.fromAccount, input.from)
         .select(config.bank.transfer.toAccount, input.to)
         .click(config.bank.transfer.continueBtn)
         .wait(config.bank.transfer.validateForm)
         .click(config.bank.transfer.confirmBtn)
+        .wait(3000)
         .end()
         .then()
     })
